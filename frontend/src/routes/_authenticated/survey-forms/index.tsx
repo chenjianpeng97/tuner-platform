70fef5b4eef133678508094156734585280477b0
Tuner <chenjianpeng97@outlook.com>
2026-03-04 14:48:18 +0800

    refactor:格式化

--- frontend/src/routes/_authenticated/survey-forms/index.tsx
+++ frontend/src/routes/_authenticated/survey-forms/index.tsx
@@ -2,5 +2,5 @@ import { createFileRoute } from '@tanstack/react-router'
 import { SurveyForms } from '@/features/survey-forms'
 
 export const Route = createFileRoute('/_authenticated/survey-forms/')({
-  component: SurveyForms,
+    component: SurveyForms,
 })
